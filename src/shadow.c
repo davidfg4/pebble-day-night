c46cab9c1c573dc0165e1b0e5cdcdd8147938446
David Gianforte <david@gianforte.org>
2015-09-17 20:37:29 -0600

    update for SDK 3 to compile on basalt

--- src/shadow.c
+++ src/shadow.c
@@ -4,6 +4,8 @@
 #define STR_SIZE 20
 #define TIME_OFFSET_PERSIST 1
 #define REDRAW_INTERVAL 15
+#define WIDTH 144
+#define HEIGHT 72
 
 static Window *window;
 static TextLayer *time_text_layer;
@@ -11,7 +13,7 @@ static TextLayer *date_text_layer;
 static GBitmap *world_bitmap;
 static Layer *canvas;
 // this is a manually created bitmap, of the same size as world_bitmap
-static GBitmap image;
+static GBitmap *image;
 static int redraw_counter;
 // s is set to memory of size STR_SIZE, and temporarily stores strings
 char *s;
@@ -44,20 +46,20 @@ static void draw_earth() {
   int sun_y = -sin_lookup((day_of_year - 0.2164) * TRIG_MAX_ANGLE) * .26 * .25;
   // ##### draw the bitmap
   int x, y;
-  for(x = 0; x < image.bounds.size.w; x++) {
-    int x_angle = (int)((float)TRIG_MAX_ANGLE * (float)x / (float)(image.bounds.size.w));
-    for(y = 0; y < image.bounds.size.h; y++) {
-      int byte = y * image.row_size_bytes + (int)(x / 8);
-      int y_angle = (int)((float)TRIG_MAX_ANGLE * (float)y / (float)(image.bounds.size.h * 2)) - TRIG_MAX_ANGLE/4;
+  for(x = 0; x < WIDTH; x++) {
+    int x_angle = (int)((float)TRIG_MAX_ANGLE * (float)x / (float)(WIDTH));
+    for(y = 0; y < HEIGHT; y++) {
+      int byte = y * gbitmap_get_bytes_per_row(image) + (int)(x / 8);
+      int y_angle = (int)((float)TRIG_MAX_ANGLE * (float)y / (float)(HEIGHT * 2)) - TRIG_MAX_ANGLE/4;
       // spherical law of cosines
       float angle = ((float)sin_lookup(sun_y)/(float)TRIG_MAX_RATIO) * ((float)sin_lookup(y_angle)/(float)TRIG_MAX_RATIO);
       angle = angle + ((float)cos_lookup(sun_y)/(float)TRIG_MAX_RATIO) * ((float)cos_lookup(y_angle)/(float)TRIG_MAX_RATIO) * ((float)cos_lookup(sun_x - x_angle)/(float)TRIG_MAX_RATIO);
-      if ((angle < 0) ^ (0x1 & (((char *)world_bitmap->addr)[byte] >> (x % 8)))) {
+      if ((angle < 0) ^ (0x1 & (((char *)gbitmap_get_data(world_bitmap))[byte] >> (x % 8)))) {
         // white pixel
-        ((char *)image.addr)[byte] = ((char *)image.addr)[byte] | (0x1 << (x % 8));
+        ((char *)gbitmap_get_data(image))[byte] = ((char *)gbitmap_get_data(image))[byte] | (0x1 << (x % 8));
       } else {
         // black pixel
-        ((char *)image.addr)[byte] = ((char *)image.addr)[byte] & ~(0x1 << (x % 8));
+        ((char *)gbitmap_get_data(image))[byte] = ((char *)gbitmap_get_data(image))[byte] & ~(0x1 << (x % 8));
       }
     }
   }
@@ -65,7 +67,7 @@ static void draw_earth() {
 }
 
 static void draw_watch(struct Layer *layer, GContext *ctx) {
-  graphics_draw_bitmap_in_rect(ctx, &image, image.bounds);
+  graphics_draw_bitmap_in_rect(ctx, image, gbitmap_get_bounds(image));
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -110,34 +112,27 @@ static void app_message_inbox_received(DictionaryIterator *iterator, void *conte
 
 static void window_load(Window *window) {
 #ifdef BLACK_ON_WHITE
-  window_set_background_color(window, GColorWhite);
+  GColor background_color = GColorWhite;
+  GColor foreground_color = GColorBlack;
 #else
-  window_set_background_color(window, GColorBlack);
+  GColor background_color = GColorBlack;
+  GColor foreground_color = GColorWhite;
 #endif
+  window_set_background_color(window, background_color);
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   time_text_layer = text_layer_create(GRect(0, 72, 144-0, 168-72));
-#ifdef BLACK_ON_WHITE
-  text_layer_set_background_color(time_text_layer, GColorWhite);
-  text_layer_set_text_color(time_text_layer, GColorBlack);
-#else
-  text_layer_set_background_color(time_text_layer, GColorBlack);
-  text_layer_set_text_color(time_text_layer, GColorWhite);
-#endif
+  text_layer_set_background_color(time_text_layer, background_color);
+  text_layer_set_text_color(time_text_layer, foreground_color);
   text_layer_set_font(time_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   text_layer_set_text(time_text_layer, "");
   text_layer_set_text_alignment(time_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(time_text_layer));
 
   date_text_layer = text_layer_create(GRect(0, 130, 144-0, 168-130));
-#ifdef BLACK_ON_WHITE
-  text_layer_set_background_color(date_text_layer, GColorWhite);
-  text_layer_set_text_color(date_text_layer, GColorBlack);
-#else
-  text_layer_set_background_color(date_text_layer, GColorBlack);
-  text_layer_set_text_color(date_text_layer, GColorWhite);
-#endif
+  text_layer_set_background_color(date_text_layer, background_color);
+  text_layer_set_text_color(date_text_layer, foreground_color);
   text_layer_set_font(date_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   text_layer_set_text(date_text_layer, "");
   text_layer_set_text_alignment(date_text_layer, GTextAlignmentCenter);
@@ -146,8 +141,7 @@ static void window_load(Window *window) {
   canvas = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
   layer_set_update_proc(canvas, draw_watch);
   layer_add_child(window_layer, canvas);
-  image = *world_bitmap;
-  image.addr = malloc(image.row_size_bytes * image.bounds.size.h);
+  image = gbitmap_create_blank(GSize(WIDTH, HEIGHT), GBitmapFormat1Bit);
   draw_earth();
 }
 
@@ -155,7 +149,7 @@ static void window_unload(Window *window) {
   text_layer_destroy(time_text_layer);
   text_layer_destroy(date_text_layer);
   layer_destroy(canvas);
-  free(image.addr);
+  gbitmap_destroy(image);
 }
 
 static void init(void) {
